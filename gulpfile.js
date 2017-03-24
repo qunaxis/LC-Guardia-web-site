1e26963ad8e8ad41dad3a783b1f01060ccfe2524
Dmitry Podlesnykh <qunaxis@gmail.com>
2017-03-24 16:44:43 +0300

    Add 1 screen and footer + small adaptive
    WARN: 7 errors in console, need to fix

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,7 @@ gulp.task('headersass', function() {
 gulp.task('libs', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-carousel/slick/slick.min.js',
 		// 'app/libs/magnific-popup/magnific-popup.min.js'
 		])
 		.pipe(concat('libs.min.js'))
